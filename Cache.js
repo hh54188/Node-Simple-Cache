085062ae848a97e8b5ccead03237db5df9852e95
invegas <juststayinvegas@gmail.com>
2013-11-11 20:20:00 +0800

    rollback

--- Cache.js
+++ Cache.js
@@ -1,93 +1,15 @@
 var LRU = require("./algorithm/LRU");
 var LFU = require("./algorithm/LFU");
 
-var lastAlgo = null, lastMaxsize = null, globalCache = null;;
-
-var usedAlgo = [];
-var usedRate = {};
-var stopSwitch = false;
-
-var algorithm = {
-    "LRU": LRU,
-    "LFU": LFU
-}
-
-var hitRate = {
-    total: 0,
-    suc: 0
-}
-
-var algoSwitchTo = function (name) {
-    console.log("Final algorithm------>", name);
-    globalCache.queue = generateQueue(name, lastMaxsize);
-
-    for (var key in globalCache.cache) {
-        if (globalCache.cache[key]) {
-            var returnNode = globalCache.queue.insert(key);
-            globalCache.cache[key].node = returnNode.node;
-        }
-    }
-}
-
-var algoSwitch = function () {
-    if (stopSwitch) return;
-
-    if (hitRate.total > 100 * 100 ) {
-        console.log("------>algoSwitch<------");
-        console.log("Last algorithm:", lastAlgo);
-        console.log("Hit Rate:", hitRate.suc / parseFloat(hitRate.total));
-
-        usedRate[lastAlgo] = hitRate.suc / parseFloat(hitRate.total);
-
-        for (var name in algorithm) {
-            if (!usedRate[name]) {
-                console.log("switch to------->", name);
-
-                lastAlgo = name;
-                globalCache.queue = generateQueue(name, lastMaxsize);
-
-                for (var key in globalCache.cache) {
-                    if (globalCache.cache[key]) {
-                        var returnNode = globalCache.queue.insert(key);
-                        globalCache.cache[key].node = returnNode.node;
-                    }
-                }
-
-                hitRate = {
-                    total: 0,
-                    suc: 0
-                }
-                console.log("hitRate reset------>");
-                console.log("Give value complete");
-
-                return;
-            }
-        }
-
-        stopSwitch = true;
-
-        // 如果算法都用过了，找出命中率最高的算法
-
-        var maxRate = 0;
-        var maxName = null
-        for (var key in usedRate) {
-            if (usedRate[key] > maxRate) {
-                maxRate = usedRate[key];
-                maxName = key;
-            }
-                
-        }
-
-        
-        console.log("The highest algorithm:", maxName, maxRate);
-        algoSwitchTo(maxName);
-    }
-}
+var lastAlgo = null, lastMaxsize = null;
 
 var generateQueue = function (name, maxsize) {
+    var algorithm = {
+        "LRU": LRU,
+        "LFU": LFU
+    }
 
     lastAlgo = name || "LRU";
-    usedAlgo.push(lastAlgo);
     lastMaxsize = maxsize;
 
     var Queue = (name && algorithm[name])? algorithm[name]: LRU;
@@ -130,7 +52,6 @@ var set = function (key, value, expire) {
 }
 
 var get = function (key) {
-    hitRate.total++;
     var _cache = this.cache;
     var _queue = this.queue;
 
@@ -147,9 +68,6 @@ var get = function (key) {
             // 已经使用过，更新队列
             _queue.update(node);
 
-            hitRate.suc++;
-            algoSwitch();
-
             return _cache[key].value;
 
         // 如果已经过期
@@ -157,12 +75,10 @@ var get = function (key) {
 
             // 从队列中删除
             _queue.del(node);
-            algoSwitch();
             return null
         }
 
     } else {
-        algoSwitch();
         return null;
     }
 
@@ -179,7 +95,6 @@ var print = function () {
 }
 
 var createCache = function (alg_name, maxsize) {
-    if (alg_name) stopSwitch = true;
 
     var obj =  {
         cache: {},
@@ -190,8 +105,6 @@ var createCache = function (alg_name, maxsize) {
         clear: clear,
         print: print
     }
-
-    globalCache = obj;
 
     setInterval(function () {
         var cache = obj.cache;
